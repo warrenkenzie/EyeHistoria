c932b096a6907d777eb1e3d9dffc36473dccc30f
Your own username <Your own email>
2023-10-30 12:49:33 +0800

    able to put in the num of symptoms to each diseases

--- EyeHistoria/Controllers/HomeController.cs
+++ EyeHistoria/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using EyeHistoria.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection.Metadata.Ecma335;
 
@@ -7,6 +8,13 @@ namespace EyeHistoria.Controllers
 {
     public class HomeController : Controller
     {
+        // put the symptoms to find diagnosis
+        List<string> list_of_symptoms = new List<string>();
+
+        // dummy diseases for diagnosis
+        List<string> cataract = new List<string> { "eyeRedness", "eyeSwelling" };
+        
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -38,12 +46,63 @@ namespace EyeHistoria.Controllers
         [HttpPost]
         public ActionResult SubmitDiagnosis(IFormCollection formData)
         {
+            // initialise list of diseases
+            Dictionary<string, List<string>> list_of_diseases = Generate_list_of_diseases();
+            if(!list_of_diseases.Any()) //  checks if list_of_diseases is empty, if not generate
+            {
+                Generate_list_of_diseases();
+            }
+            List<int> checks = new List<int>();
+
+            // put the list of symptoms in a list
             string eyeRedness = formData["eyeRedness"].ToString();
             string eyeItch = formData["eyeItch"].ToString();
             string eyeSwelling = formData["eyeSwelling"].ToString();
 
-            ViewData["diagnosis"] = string.Format(eyeRedness + " " + eyeItch + " " + eyeSwelling);
+            list_of_symptoms.Add(eyeRedness);
+            list_of_symptoms.Add(eyeItch);
+            list_of_symptoms.Add(eyeSwelling);
+
+            int index = list_of_symptoms.Count();
+            int num_of_true = 0;
+            // goes through the list of diseases
+            foreach (KeyValuePair<string, List<string>> disease in list_of_diseases)
+            {
+                // iterates through symptoms sent
+                for (int i = 0; i < list_of_symptoms.Count(); i++)
+                {
+                    // iterates through a disease's symptoms
+                    for (int j = 0;j < disease.Value.Count(); j++)
+                    {
+                        // goes through the list symptoms of a disease and then true if the syptom matches list_of_symptoms
+                        if (list_of_symptoms[i] == disease.Value[j])
+                        {
+                            num_of_true++;
+                            break;
+                        }
+                    }
+                   
+                }
+                checks.Add(num_of_true);
+                num_of_true = 0;
+            }
+            Console.WriteLine(checks.ToString());
+            ViewData["diagnosis"] = num_of_true;
             return View();
+        }
+
+        // generates a dict of diseases
+        Dictionary<string, List<string>> Generate_list_of_diseases()
+        {
+            Dictionary<string, List<string>> list_of_diseases = new Dictionary<string, List<string>>();
+            List<string> cataract = new List<string> { "eyeRedness", "eyeItch", "eyeSwelling" };
+            List<string> blindness = new List<string> { "eyeSwelling" };
+            List<string> swollenEyes = new List<string> { "eyeSwelling", "eyeItch" };  
+            list_of_diseases.Add("cataract", cataract);
+            list_of_diseases.Add("blindness", blindness);
+            list_of_diseases.Add("swollenEyes", swollenEyes);
+
+            return list_of_diseases;
         }
     }
 }
