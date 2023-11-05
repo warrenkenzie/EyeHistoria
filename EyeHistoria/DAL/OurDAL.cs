dfd39481c1052e7e311e7bff7c315191561bdc15
Tan Jin Wen <S10242734@connect.np.edu.sg>
2023-11-05 12:52:02 +0800

    DAL & Administrative Initiation

---
+++ EyeHistoria/DAL/OurDAL.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using EyeHistoria.Models;
+using Humanizer;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EyeHistoria.DAL
+{
+    public class OurDAL
+    {
+        private IConfiguration Configuration { get; }
+        private SqlConnection conn;
+        //Constructor
+        public OurDAL()
+        {
+            //Read ConnectionString from appsettings.json file
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+            string strConn = Configuration.GetConnectionString(
+            "EyeHistoriaConnectionString");
+            //Instantiate a SqlConnection object with the
+            //Connection String read.
+            conn = new SqlConnection(strConn);
+        }
+
+        public List<Symptoms> GetAllSymptoms()
+        {
+            //Create a SqlCommand object from connection object
+            SqlCommand cmd = conn.CreateCommand();
+            //Specify the SELECT SQL statement 
+            cmd.CommandText = @"SELECT * FROM Symptoms";
+            //Open a database connection
+            conn.Open();
+            //Execute the SELECT SQL through a DataReader
+            SqlDataReader reader = cmd.ExecuteReader();
+            //Read all records until the end, save data into a staff list
+            List<Symptoms> symptomsList = new List<Symptoms>();
+            while (reader.Read())
+            {
+                symptomsList.Add(
+                new Symptoms
+                {
+                    SymptomID = reader.GetInt32(0), //0: 1st column
+                    Name = reader.GetString(1) //1: 2nd column
+                }
+                );
+            }
+            //Close DataReader
+            reader.Close();
+            //Close the database connection
+            conn.Close();
+            return symptomsList;
+        }
+
+    }
+}
---
+++ EyeHistoria/Models/Symptoms.cs
@@ -0,0 +1,9 @@
+﻿namespace EyeHistoria.Models
+{
+    public class Symptoms
+    {
+        public int SymptomID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
