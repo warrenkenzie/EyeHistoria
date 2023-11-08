50d31bcf6fe78b53c74eefa557ae625dd4af28fc
MyatKo <myatkonkaungthantsoe@gmail.com>
2023-11-08 21:57:10 +0800

    Validation for question

--- EyeHistoria/Models/Question.cs
+++ EyeHistoria/Models/Question.cs
@@ -1,15 +1,41 @@
-﻿namespace EyeHistoria.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EyeHistoria.Models
 {
     public class Question
     {
         public int QuestionID { get; set; }
+
+        [Required(ErrorMessage = "Question is required.")]
+        [Display(Name = "Question")]
         public string QuestionText { get; set; }
+
+        [Required(ErrorMessage = "Question type is required.")]
+        [Display(Name = "Question Type")]
         public string Type { get; set; }
+
+        [Required(ErrorMessage = "Question category is required.")]
+        [Display(Name = "Question Category")]
         public string Category { get; set; }
+
+        [Required(ErrorMessage = "Symptom ID is required.")]
+        [Display(Name = "Symptom ID")]
         public int SymptomID { get; set; }
+
+        [Required(ErrorMessage = "Symptom name is required.")]
+        [Display(Name = "Symptom Name")]
         public string SymptomName { get; set; }
+
+        [Required(ErrorMessage = "Admin ID is required.")]
+        [Display(Name = "Admin ID")]
         public int AdminID { get; set; }
+
+        [Required(ErrorMessage = "Last modified by is required.")]
+        [Display(Name = "Last Modified By")]
         public string LastModifiedBy { get; set; }
+
+        [Required(ErrorMessage = "Date is required.")]
+        [Display(Name = "Date")]
         public DateTime Date { get; set; }
         public int? FollowUpID { get; set; }
 
