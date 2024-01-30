e63f91e83be73d2493e5c7baad875a8736b8552a
Tan Jin Wen <S10242734@connect.np.edu.sg>
2024-01-30 09:58:28 +0800

    Initiating Process DAL

---
+++ EyeHistoria/DAL/ProcessDAL.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SqlClient;
+using EyeHistoria.Models;
+
+namespace EyeHistoria.DAL
+{
+    public class ProcessDAL
+    {
+        private IConfiguration Configuration { get; }
+        private SqlConnection conn;
+        //Constructor
+        public ProcessDAL()
+        {
+            //Read ConnectionString from appsettings.json file
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+            string strConn = Configuration.GetConnectionString(
+            "ProcessConnectionString");
+            //Instantiate a SqlConnection object with the
+            //Connection String read.
+            conn = new SqlConnection(strConn);
+        }
+    }
+}
